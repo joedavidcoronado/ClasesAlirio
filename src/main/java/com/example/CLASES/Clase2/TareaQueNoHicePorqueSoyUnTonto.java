64aa5295b02af5fab754e5b4e24ccac3696f1331
joedavidcoronado <joe.d.coronado.cgmail.com>
2026-04-18 13:16:11 -0400

    Subida de avance Alirio

---
+++ src/main/java/com/example/CLASES/Clase2/TareaQueNoHicePorqueSoyUnTonto.java
@@ -0,0 +1,74 @@
+package com.example.CLASES.Clase2;
+
+public class TareaQueNoHicePorqueSoyUnTonto {
+    public static void main(String[] args) {
+
+        /*
+        //EJERCICIO #1:                                               > y <
+        //Crea un programa que tenga una variable edad.
+        //El programa debe mostrar:
+        //      "Es mayor de edad" si la edad es 18 o más
+        //      "Es menor de edad" si es menor de 18
+
+        int edad = 18;
+
+        if (edad >= 18) {
+            //System.out.println("Es mayor de edad");
+        } else {
+            //System.out.println("Es menor de edad");
+        }
+
+        //EJERCICIO #2:
+        //Crea una variable int numero.
+        //El programa debe decir:
+        //"El número es positivo"
+        //"El número es negativo"
+        //"El número es cero"
+        //Usando if.
+
+        int numero = 20;
+        if (numero < 0) {
+         //   System.out.println("Es negativo");
+            return;
+        }
+        if (numero == 0) {
+           // System.out.println("El numero es cero");
+            return;
+        }
+        if (numero > 0) {
+            //System.out.println("El numero es positivo");
+            return;
+        }
+
+
+        //EJERCICIO #3:
+        // Nota aprobada o reprobada
+        //Crea una variable double nota.
+        //El programa debe mostrar:
+        //"Aprobado" si la nota es mayor o igual a 51
+        //"Reprobado" si es menor.
+
+        double nota = 45.00 ;
+        if (nota < 51.00 ){
+            System.out.println("estas reprobado");
+        }
+        if (nota > 51.00) {
+            System.out.println("estas aprobado");
+        }
+
+
+        //EJERCICIO #4:
+       // Mostrar números del 1 al 10
+       // Usa un for para mostrar en pantalla:
+
+        1 al 10 */
+
+        for(int contador =1 ; contador < 11 ; contador ++) {
+            System.out.println(contador);
+        }
+
+        /// ////////////////
+    }
+}
+
+
