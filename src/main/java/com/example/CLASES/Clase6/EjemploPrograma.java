af4efbf32be9dfe8390a0e3e40119648847ddebe
joedavidcoronado <joe.d.coronado.c@gmail.com>
2026-07-22 10:59:22 -0400

    Clases de Samuel jejeje

---
+++ src/main/java/com/example/CLASES/Clase6/EjemploPrograma.java
@@ -0,0 +1,49 @@
+package com.example.CLASES.Clase6;
+
+public class EjemploPrograma {
+    public static void main(String[] args) {
+
+        /*
+        boolean prendido = true;
+        while (prendido == true){
+            //MENU
+            System.out.println("---------------------------------------------------------");
+            System.out.println("Hola, este es tu menu, selecciona una opcion");
+            System.out.println("1. Imprime Hola");
+            System.out.println("2. Tablas de Multiplicar");
+            System.out.println("3. Suma");
+            System.out.println("4. Restar");
+            System.out.println("5. Salir");
+            System.out.println("---------------------------------------------------------");
+        }
+        */
+
+        //Cuarta Estructura de control - Switch (cambiar)
+
+        /*
+        char caracter = '-';
+
+        switch(caracter){
+            case '+':
+                //---------------------------------------------
+
+
+                ACA DENTRO PUEDO HACER LO QUE YO QUIERA
+
+
+                //---------------------------------------------
+                break;
+            case '-':
+                System.out.println("Este es el caso 2");
+                break;
+            case '/':
+                System.out.println("Este es el caso 2");
+                break;
+            default:
+                System.out.println("No es ningun caso");
+        }
+
+         */
+
+    }
+}
---
+++ src/main/java/com/example/CLASES/Clase6/Main.java
@@ -0,0 +1,59 @@
+package com.example.CLASES.Clase6;
+
+public class Main {
+    public static void main(String[] args) {
+        // NO MAS DE ACAAAAAAAAAAA ////////////////////////////////////////////////////////////////////
+
+        /*
+        int numero = 20;
+
+        //Rangos entre condiciones
+        if(numero > 19 || numero < 21){
+            System.out.println("El numero es correcto");
+        }else{
+            System.out.println("El numero es incorrecto");
+        }
+        */
+        /*
+        int numeroPrueba = 0;
+
+        //Segunda estructura de control: Bucles (for significa "para")
+        //   inicias en...     paras cuando...    aumentas...
+        for(int contador = 0; contador < 10; contador++){
+            numeroPrueba++;
+            if(numeroPrueba % 2 == 0){
+                System.out.println("El numero " + numeroPrueba + " es par");
+            }else{
+                System.out.println("El numero " + numeroPrueba + " es impar");
+            }
+        }
+
+        System.out.println(numeroPrueba);
+        */
+
+        /*
+        //EJERCICIO DE TABLAS DE MULTIPLICAR
+        int tablaSiete = 7;
+
+        for(int contador = 10; contador > 0; contador--){
+            System.out.println(  tablaSiete + " x " + contador + " = " + (tablaSiete * contador)   );
+        }
+        */
+
+
+        //Tercera estructura de control: While - "Mientras"
+        boolean condicion = true;
+        int variableEjemplo = 0;
+
+        while (condicion == true){
+            variableEjemplo++;
+            System.out.println(variableEjemplo);
+            if (variableEjemplo == 10){
+                condicion = false;
+            }
+        }
+
+
+        // NO MAS DE ACAAAAAAAAAAA ////////////////////////////////////////////////////////////////////
+    }
+}
